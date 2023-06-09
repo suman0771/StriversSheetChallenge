0b85ed6aad99187a21e8f46b513190e89f190a08
Suman Moond <sumanmoond2021@gmail.com>
2023-06-09 15:41:51 +0530

    Kadane's Algorithm

--- Day2/kadaneAlgorithm.cpp
+++ Day2/kadaneAlgorithm.cpp
@@ -47,12 +47,37 @@ int optimize(vector<int> &arr){
     return maxi;
 }
 
-
+vector<int> printSubarray(vector<int> &arr){
+    int n=arr.size();
+    vector<int> ans;
+    int maxi=INT_MIN,sum=0,start=-1,Astart=-1,Aend=-1;
+    for(int i=0;i<n;i++){
+        if(sum==0) {start=i;}
+        sum+=arr[i];
+        
+        if(sum>maxi){
+            maxi=sum;
+           Astart=start;
+           Aend=i; 
+        }
+        if(sum<0) {sum=0;}
+    }
+    for(int i=Astart;i<Aend;i++){
+        ans.push_back(arr[i]);
+    }
+    return ans;
+}
 
 int main(){
     vector<int> arr={-2,-3,4,-1,-2,1,5,-3};
     // cout<<subarraySum(arr);
     // cout<<betterSolution(arr);
-    cout<<optimize(arr);
+    // cout<<optimize(arr);
+    vector<int> res=printSubarray(arr);
+    cout<<"[ ";
+    for(auto it:res){
+        cout<<it<<",";
+    }
+    cout<<"]";
 
 }
